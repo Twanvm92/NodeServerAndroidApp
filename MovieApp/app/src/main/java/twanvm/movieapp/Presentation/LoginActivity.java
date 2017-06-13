e0364de11b64d96420c895856b5a3afe978deacd
Twan van Maastricht <ajmaastr@avans.nl>
2017-06-13 20:06:43 +0200

    Set up Volley and constants

--- MovieApp/app/src/main/java/twanvm/movieapp/Presentation/LoginActivity.java
+++ MovieApp/app/src/main/java/twanvm/movieapp/Presentation/LoginActivity.java
@@ -1,4 +1,4 @@
-package twanvm.movieapp;
+package twanvm.movieapp.Presentation;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -31,6 +31,8 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import twanvm.movieapp.R;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
